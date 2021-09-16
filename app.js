ddc2cf7e01ce70ac21d8aa64d4e68bf666dc1bb4
Denys Gaspar <denysp.gaspar@gmail.com>
2021-09-15 21:54:46 -0300

    fix: removing unecessary files

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
+const mongoose = require("mongoose");
 const express = require('express');
+
 const app = express();
 
 const port = 4001;
 
 app.listen(port, function () {
     console.log('Server is running on Port: ' + port);
-
-    const mongoose = require("mongoose");
 
     const dbUser = {
         login: 'dgaspar-teste',
--- user-information.js
+++
@@ -1,8 +0,0 @@
-require('dotenv').config()
-
-const dbUser = {
-    login: process.env.DB_USER,
-    password: process.env.DB_PASSWORD
-};
-
-export { dbUser };
